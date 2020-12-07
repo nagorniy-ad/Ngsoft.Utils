da4c565cdcaf2a3c7028dd51be0abf6618999a1a
Александр Нагорный <nagorniy.ad@gmail.com>
2020-12-07 19:50:44 +1000

    Add project files.

---
+++ Ngsoft.Utils.Tests/UXHelperTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Ngsoft.Utils.Tests
+{
+    public class UXHelperTests
+    {
+        [Fact]
+        public void GetDeclension_ReturnsExpectedValues()
+        {
+            // Arrange
+            var nominative = "заказ";
+            var genetive = "заказа";
+            var plural = "заказов";
+
+            // Act
+            var value1 = UXHelper.GetDeclension(0, nominative, genetive, plural);
+            var value2 = UXHelper.GetDeclension(1, nominative, genetive, plural);
+            var value3 = UXHelper.GetDeclension(2, nominative, genetive, plural);
+            var value4 = UXHelper.GetDeclension(5, nominative, genetive, plural);
+            var value5 = UXHelper.GetDeclension(21, nominative, genetive, plural);
+            var value6 = UXHelper.GetDeclension(2011, nominative, genetive, plural);
+            var value7 = UXHelper.GetDeclension(2351, nominative, genetive, plural);
+            var value8 = UXHelper.GetDeclension(2315, nominative, genetive, plural);
+            var value9 = UXHelper.GetDeclension(-158, nominative, genetive, plural);
+            var value10 = UXHelper.GetDeclension(-2315, nominative, genetive, plural);
+
+            // Assert
+            Assert.Equal(plural, value1);
+            Assert.Equal(nominative, value2);
+            Assert.Equal(genetive, value3);
+            Assert.Equal(plural, value4);
+            Assert.Equal(nominative, value5);
+            Assert.Equal(plural, value6);
+            Assert.Equal(nominative, value7);
+            Assert.Equal(plural, value8);
+            Assert.Equal(plural, value9);
+            Assert.Equal(plural, value10);
+        }
+    }
+}
---
+++ Ngsoft.Utils/UX/UXHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Ngsoft.Utils
+{
+    public static class UXHelper
+    {
+        /// <summary>
+        /// Возвращает слова в падеже, зависимом от заданного числа.
+        /// <para>Основано на: <a href="https://devnuances.com/c_sharp/cklonenie-sushhestvitelnyih-posle-chislitelnyih-v-c_sharp/"/></para>
+        /// </summary>
+        /// <param name="number">Число, от которого зависит выбранное слово.</param>
+        /// <param name="nominative">Именительный падеж слова. Например "день".</param>
+        /// <param name="genetive">Родительный падеж слова. Например "дня".</param>
+        /// <param name="plural">Множественное число слова. Например "дней".</param>
+        /// <returns>Слово в падеже, соответствующем числу.</returns>
+        public static string GetDeclension(int number, string nominative, string genetive, string plural)
+        {
+            number %= 100;
+            if ((number >= 11 && number <= 19) || (number <= -11 && number >= -19))
+            {
+                return plural;
+            }
+            var i = number % 10;
+            if (i == 1 || i == -1)
+            {
+                return nominative;
+            }
+            else if (i == 2 || i == -2 || i == 3 || i == -3 || i == 4 || i == -4)
+            {
+                return genetive;
+            }
+            else
+            {
+                return plural;
+            }
+        }
+    }
+}
